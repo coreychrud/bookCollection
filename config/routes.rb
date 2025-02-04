ed4a2afaee905cc18d5d8d1fbf857e91e33ff65c
coreychrud <coreychrud@tmau.edu>
2025-02-04 00:40:18 +0000

    updated scaffolding stuff

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,16 @@
 Rails.application.routes.draw do
+  resources :user_books
+  resources :users
   resources :books
   root 'books#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  resources :books do
+    member do
+      get :delete
+    end
+  end
 end
