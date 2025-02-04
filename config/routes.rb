8119ae1d7ce0bc6c03c8780d4280f4e71eefa886
coreychrud <coreychrud@tmau.edu>
2025-02-04 01:07:07 +0000

    updated user stuff

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,14 @@
 Rails.application.routes.draw do
   resources :user_books
   resources :users
-  resources :books
-  root 'books#index'
+  resources :books do
+    member do
+      get 'delete', to: 'books#delete', as: 'delete'
+    end
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
-
-  resources :books do
-    member do
-      get :delete
-    end
-  end
+  root "user_books#index"
 end
